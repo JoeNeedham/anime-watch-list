51890d49f2f6e99fa6d2eade05d31c4460b521fc
joeneedum <joeneedum@gmail.com>
2022-06-01 16:25:24 -0400

    commit

---
+++ src/components/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './home.css'
+
+
+function Home() {
+    return (
+        <div className="wrapper">
+            <div className="title">
+                <h1>ANIME HOARDERS</h1>
+                <h2>WHAT ARE YOU WATCHING NEXT?</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Home
