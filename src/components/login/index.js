58c2e20ebbddf2f578a8d29bfadb0547d3b18a86
joeneedum <joeneedum@gmail.com>
2022-06-03 17:54:52 -0400

    login page

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { Navbar } from '../index'
+import './login.css'
 
 
 function Login() {
     return (
         <div className="wrapper">
             <Navbar />
+            <div>
+                <form action="" method="post">
+                    <div class="form-field">
+                        <label for="email">Email Address</label>
+                        <input type="email" id="email" name="email" />
+                    </div>
+                    
+                    <div class="form-field">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" />
+                    </div>
+                    
+                    <input type="submit" value="Sign In" class="button" />
+                    <a href="#0" class="forgot-password">Forgot your password?</a>
+                </form>          
+            </div>
         </div>
     )
 }
